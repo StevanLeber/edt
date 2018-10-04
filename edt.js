4fc265afcce7ba9e542bfc573b5e28f027f6ce29
stevan <stevanleber@gmail.com>
2018-10-04 17:00:13 +0200

    update server

--- edt.js
+++ edt.js
@@ -2,29 +2,29 @@ const Discord = require('discord.js')
 const client = new Discord.Client()
 const planning = client.channels.find('id', 493806625145225222)
 
-function longPoll_feed () {
-	//make another request
-	$.ajax({
-			cache: false,
-			dataType: 'json',
-			type: "GET",
-			url: "https://planning.univ-ubs.fr/direct/gwtdirectplanning/rss?data=bd72d825015315fe3d08f214effdde8df232d2abbea78b6a952dbe726aeb10c6dbbaf456c40409e52688752fc2b9ac75f66f57cac8cdd39f63398f4ea50c44b7061f7968f1d954af",
-			error: function () {
-				//don't flood the servers on error, wait 10 seconds before retrying
-				setTimeout(longPoll_feed, 10*1000);
-			},
-			success: function (json) {
-				display_event(json);
-
-				//if everything went well, begin another request immediately
-				//the server will take a long time to respond
-				//how long? well, it will wait until there is another message
-				//and then it will return it to us and close the connection.
-				//since the connection is closed when we get data, we longPoll again
-				longPoll_feed();
-			}
-		});
-}
+// function longPoll_feed () {
+// 	//make another request
+// 	$.ajax({
+// 			cache: false,
+// 			dataType: 'json',
+// 			type: "GET",
+// 			url: "https://planning.univ-ubs.fr/direct/gwtdirectplanning/rss?data=bd72d825015315fe3d08f214effdde8df232d2abbea78b6a952dbe726aeb10c6dbbaf456c40409e52688752fc2b9ac75f66f57cac8cdd39f63398f4ea50c44b7061f7968f1d954af",
+// 			error: function () {
+// 				//don't flood the servers on error, wait 10 seconds before retrying
+// 				setTimeout(longPoll_feed, 10*1000);
+// 			},
+// 			success: function (json) {
+// 				display_event(json);
+//
+// 				//if everything went well, begin another request immediately
+// 				//the server will take a long time to respond
+// 				//how long? well, it will wait until there is another message
+// 				//and then it will return it to us and close the connection.
+// 				//since the connection is closed when we get data, we longPoll again
+// 				longPoll_feed();
+// 			}
+// 		});
+// }
 
 function display_event (json) {
 	console.log('Update received!');
@@ -41,7 +41,7 @@ client.on('message', message => {
 
 client.on('ready', () => {
 	console.log('I am ready!');
-	longPoll_feed();
+	//longPoll_feed();
 });
 
 client.login('NDk0ODQzNTE5NjUyMDY5Mzc3.Do5cHQ.t3if75PYjxPOVf13rSwFtGPCUF4');
