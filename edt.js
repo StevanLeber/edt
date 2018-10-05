03e7d934b909e886342c7c84d551f71e7436f690
stevan <stevanleber@gmail.com>
2018-10-05 18:08:08 +0200

    test get rss

--- edt.js
+++ edt.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
+const https = require('https')
 const planning = client.channels.find('id', 493806625145225222)
 
 // function longPoll_feed () {
@@ -42,6 +43,20 @@ client.on('message', message => {
 client.on('ready', () => {
 	console.log('I am ready!');
 	client.channels.get('493806625145225222').send("coucou!")
+	https.get("https://planning.univ-ubs.fr/direct/gwtdirectplanning/rss?data=bd72d825015315fe3d08f214effdde8df232d2abbea78b6a952dbe726aeb10c6dbbaf456c40409e5da98cf1172bf16b81a2e6e73a2ee14158758a151cfe0fb9a061f7968f1d954af", (resp) => {
+  let data = '';
+
+  // A chunk of data has been recieved.
+  resp.on('data', (chunk) => {
+    data += chunk;
+  });
+
+  // The whole response has been received. Print out the result.
+  resp.on('end', () => {
+    console.log(data);
+    console.log(JSON.parse(data).explanation);
+  });
+
 	//longPoll_feed();
 });
 
